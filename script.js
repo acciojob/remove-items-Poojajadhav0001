bcc2729ffb4d07a1cc0ce17843630eee235212fa
Pooja Jadhav <pgjadhav603@gmail.com>
2025-09-26 11:40:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function removeColor() {
+      const select = document.getElementById("colorSelect");
+      // निवडलेला index शोधतो
+      const selectedIndex = select.selectedIndex;
+
+      if (selectedIndex !== -1) {
+        // निवडलेला option remove करतो
+        select.remove(selectedIndex);
+      } else {
+        alert("Please select a color to remove!");
+      }
+    }
